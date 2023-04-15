f4d4e21e7b165cbc599986506971f0d131c6c687
somrat sorkar <themefisher.dev@gmail.com>
2023-04-15 12:50:49 +0600

    initialize project

---
+++ README.md
@@ -0,0 +1,27 @@
+# Gulpplate
+
+Gulpplate is a free starter template built with Gulp and TailwindCSS, providing everything you need to jumpstart your Gulp project and save valuable time.
+
+## Dependency
+
+- node v18+
+- npm v9.5+
+- gulp v4+
+
+## Package Installation
+
+```
+npm install
+```
+
+## Development Command
+
+```
+npm run dev
+```
+
+## Build Command
+
+```
+npm run build
+```
---
+++ source/scripts/main.js
@@ -0,0 +1,140 @@
+(function () {
+  "use strict";
+
+  // ########################## Preloader ##############################
+  // window.addEventListener("load", (e) => {
+  //   document.querySelector(".preloader").style.display = "none";
+  // });
+
+  // ########################## Theme switcher ##########################
+  var darkMode = false;
+  var themeSwitch = document.querySelectorAll("[data-theme-switcher]");
+  if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    darkMode = true;
+  }
+  if (localStorage.getItem("theme") === "dark") {
+    darkMode = true;
+  } else if (localStorage.getItem("theme") === "light") {
+    darkMode = false;
+  }
+  if (darkMode) {
+    document.documentElement.classList.toggle("dark");
+  }
+  document.addEventListener("DOMContentLoaded", () => {
+    [].forEach.call(themeSwitch, function (ts) {
+      ts.checked = darkMode ? true : false;
+      ts.addEventListener("click", () => {
+        document.documentElement.classList.toggle("dark");
+        localStorage.setItem(
+          "theme",
+          document.documentElement.classList.contains("dark") ? "dark" : "light"
+        );
+      });
+    });
+  });
+
+  // ####################### Testimonial Slider #########################
+  new Swiper(".testimonial-slider", {
+    spaceBetween: 24,
+    loop: true,
+    pagination: {
+      el: ".testimonial-slider-pagination",
+      type: "bullets",
+      clickable: true,
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+      },
+      992: {
+        slidesPerView: 3,
+      },
+    },
+  });
+
+  // ########################## Tab ##########################
+  function setActiveTab(tabGroup, tabName) {
+    const tabsNav = tabGroup.querySelector("[data-tab-nav]");
+    const tabsContent = tabGroup.querySelector("[data-tab-content]");
+
+    tabsNav.querySelectorAll("[data-tab]").forEach((tabNavItem) => {
+      tabNavItem.classList.remove("active");
+    });
+    tabsContent.querySelectorAll("[data-tab-panel]").forEach((tabPane) => {
+      tabPane.classList.remove("active");
+    });
+
+    const selectedTabNavItem = tabsNav.querySelector(`[data-tab="${tabName}"]`);
+    selectedTabNavItem.classList.add("active");
+    const selectedTabPane = tabsContent.querySelector(
+      `[data-tab-panel="${tabName}"]`
+    );
+    selectedTabPane.classList.add("active");
+  }
+  const tabGroups = document.querySelectorAll("[data-tab-group]");
+  tabGroups.forEach((tabGroup) => {
+    const tabsNav = tabGroup.querySelector("[data-tab-nav]");
+    const tabsNavItem = tabsNav.querySelectorAll("[data-tab]");
+    const activeTabName =
+      localStorage.getItem(`activeTabName-${tabGroup.dataset.tabGroup}`) ||
+      tabsNavItem[0].getAttribute("data-tab");
+
+    setActiveTab(tabGroup, activeTabName);
+
+    tabsNavItem.forEach((tabNavItem) => {
+      tabNavItem.addEventListener("click", () => {
+        const tabName = tabNavItem.dataset.tab;
+        setActiveTab(tabGroup, tabName);
+        localStorage.setItem(
+          `activeTabName-${tabGroup.dataset.tabGroup}`,
+          tabName
+        );
+      });
+    });
+  });
+
+  // ########################## Accordion ##########################
+  const accordion = document.querySelectorAll("[data-accordion]");
+  accordion.forEach((header) => {
+    header.addEventListener("click", () => {
+      const accordionItem = header.parentElement;
+      accordionItem.classList.toggle("active");
+    });
+  });
+
+  // ########################## Modal ##############################
+  const openModalButtons = document.querySelectorAll("[data-modal-open]");
+  const closeModalButtons = document.querySelectorAll("[data-modal-close]");
+
+  function openModal(modal) {
+    if (modal === null) {
+      return null;
+    }
+    const overlay = modal.querySelector("[data-modal-overlay]");
+    modal.style.display = "block";
+    overlay.style.display = "block";
+  }
+
+  function closeModal(modal) {
+    if (modal === null) {
+      return null;
+    }
+    const overlay = modal.querySelector("[data-modal-overlay]");
+    modal.style.display = "none";
+    overlay.style.display = "none";
+  }
+
+  openModalButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const modal = button.nextElementSibling;
+      openModal(modal);
+    });
+  });
+
+  closeModalButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const modal = button.closest("[data-modal]");
+      closeModal(modal);
+    });
+  });
+})();
